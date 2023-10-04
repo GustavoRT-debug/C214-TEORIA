c86c7758ad5d48ce35a8d9de8ce4db6253452478
Gustavo <gustavo_ramos@gec.inatel.br>
2023-10-03 23:26:27 -0300

    Alteração no servidor

--- trabalho/servidor.java
+++ trabalho/servidor.java
@@ -1,8 +1,46 @@
 package com.inatel.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ServidorRemoto {
     public String getHorarioDeAtendimento() {
-        // Implemente o código para fazer a chamada ao servidor remoto e retornar o JSON.
-        return ""; // Substitua com o JSON real retornado pelo servidor.
+        try {
+            // URL do servidor remoto que fornece o JSON de horário de atendimento
+            URL url = new URL("URL_DO_SERVIDOR"); // Substitua pela URL real do servidor
+            
+            // conexão HTTP com o servidor
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            
+            connection.setRequestMethod("GET");
+            
+            // Obtenha a resposta do servidor
+            int responseCode = connection.getResponseCode();
+            
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // A conexão foi bem-sucedida, leia o JSON da resposta
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder response = new StringBuilder();
+                String line;
+                
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+                reader.close();
+                
+                // Retorne o JSON obtido do servidor
+                return response.toString();
+            } else {
+                // Tratar erros de conexão ou resposta inválida aqui
+                return null;
+            }
+        } catch (IOException e) {
+            /
+            e.printStackTrace();
+            return null;
+        }
     }
 }
