a450ef57de0c2f33bcdfb3b899d59ff2366cc53f
Gustavo <gustavo_ramos@gec.inatel.br>
2023-11-18 23:03:26 -0300

    modificando

--- C214-1/test_calc.py
+++ C214-1/test_calc.py
@@ -1,28 +1,34 @@
 from calculadora import Calculadora
-
-def test_soma():
-    calc = Calculadora()
-    assert calc.somar(3, 5) == 8
+import unittest
 
 
-def test_subtracao():
-    calc = Calculadora()
-    assert calc.subtrair(10, 3) == 7
+class TestCalculadora(unittest.TestCase):
+    def test_soma(self):
+        calc = Calculadora()
+        result = calc.somar(3, 5)
+        self.assertEqual(result, 8)
+
+    def test_subtracao(self):
+        calc = Calculadora()
+        result = calc.subtrair(10, 3)
+        self.assertEqual(result, 7)
+
+    def test_multiplicacao(self):
+        calc = Calculadora()
+        result = calc.multiplicar(2, 4)
+        self.assertEqual(result, 8)
+
+    def test_divisao(self):
+        calc = Calculadora()
+        result = calc.dividir(10, 2)
+        self.assertEqual(result, 5)
+
+    def test_divisao_por_zero(self):
+        calc = Calculadora()
+        with self.assertRaises(ValueError) as context:
+            calc.dividir(5, 0)
+        self.assertEqual(str(context.exception), "Não é possível dividir por zero")
 
 
-def test_multiplicacao():
-    calc = Calculadora()
-    assert calc.multiplicar(2, 4) == 8
-
-
-def test_divisao():
-    calc = Calculadora()
-    assert calc.dividir(10, 2) == 5
-
-
-def test_divisao_por_zero():
-    calc = Calculadora()
-    try:
-        calc.dividir(5, 0)
-    except ValueError as e:
-        assert str(e) == "Não é possível dividir por zero"
+if __name__ == "__main__":
+    unittest.main()
